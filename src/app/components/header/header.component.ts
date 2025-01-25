223ed5333888d85a0417d6e00f0c16c00aaabd89
AmanBhanse <amanbhanse22@gmail.com>
2025-01-25 19:44:27 +0100

    Add cart button to UI

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,15 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { PrimaryButtonComponent } from '../primary-button/primary-button.component';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [PrimaryButtonComponent],
   template: `
-    <div class="bg-slate-100 px-4 py-3 shadow-md">
-      <span>Buy Wish</span>
+    <div class="bg-slate-100 px-4 py-3 shadow-md flex justify-between items-center">
+      <span class="text-xl">Buy Wish</span>
+      <app-primary-button [label]="cartTitle()" (btnClicked)="showBtnClickFromHeader()" />
     </div>
   `,
   styles: ``
 })
 export class HeaderComponent {
 
+  cartTitle = signal("Cart")
+  showBtnClickFromHeader(){
+    console.log("showBtnClickFromHeader")
+  }
 }
---
+++ src/app/components/primary-button/primary-button.component.ts
@@ -0,0 +1,22 @@
+import { Component, input, output } from '@angular/core';
+
+@Component({
+  selector: 'app-primary-button',
+  imports: [],
+  template: `
+    <button class="bg-blue-500 text-white w-full border px-5 py-2 rounded-xl shadow-md hover:opacity-90" (click)="handleButtonClick()">
+      {{label()}}
+    </button>
+  `,
+  styles: ``
+})
+export class PrimaryButtonComponent {
+  label = input('BUTTON');
+  btnClicked = output();
+
+  handleButtonClick = () =>{
+    console.log("START : handleButtonClick")
+    this.btnClicked.emit()
+    console.log("END : handleButtonClick")
+  }
+}
