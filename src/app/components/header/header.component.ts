ce66754ef52821a45dd7abf5dbddb8a6cdc62f7b
AmanBhanse <amanbhanse22@gmail.com>
2025-01-25 19:22:09 +0100

    Starter code

---
+++ src/app/components/header/header.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  imports: [],
+  template: `
+    <div class="bg-slate-100 px-4 py-3 shadow-md">
+      <span>Buy Wish</span>
+    </div>
+  `,
+  styles: ``
+})
+export class HeaderComponent {
+
+}
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts,scss}", // Scan HTML, TypeScript, and SCSS files
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+
