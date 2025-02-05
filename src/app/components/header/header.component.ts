7c9dc0ec5c0530a6327c0fc83e4d7a447eedda38
AmanBhanse <amanbhanse22@gmail.com>
2025-02-05 11:17:15 +0100

    Add icon

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,13 +2,18 @@ import { Component, inject, signal } from '@angular/core';
 import { PrimaryButtonComponent } from '../primary-button/primary-button.component';
 import { CartService } from '../../services/cart.service';
 import { RouterLink } from '@angular/router';
+import {faShoppingBag } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @Component({
   selector: 'app-header',
-  imports: [PrimaryButtonComponent, RouterLink],
+  imports: [PrimaryButtonComponent, RouterLink, FontAwesomeModule],
   template: `
     <div class="bg-slate-100 px-4 py-3 shadow-md flex justify-between items-center">
-      <div routerLink="/" class="text-4xl cursor-pointer">Buy Wish</div>
+      <div routerLink="/" class="text-4xl cursor-pointer">
+        <fa-icon class="m-3" [icon]="faShoppingBag"></fa-icon>
+        <span>Buy Wish</span>
+      </div>
       <app-primary-button 
         [label]="'Cart('+cartService.cart().length + ')'" 
         (btnClicked)="showBtnClickFromHeader()"
@@ -19,9 +24,9 @@ import { RouterLink } from '@angular/router';
   styles: ``
 })
 export class HeaderComponent {
-
-  
+  faShoppingBag = faShoppingBag;
   cartService = inject(CartService) 
+
   showBtnClickFromHeader(){
     console.log("showBtnClickFromHeader")
   }
